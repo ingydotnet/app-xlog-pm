e35f1a2a7da9af0241c22f018e87166c9e784a48
Ingy dot Net <ingy@ingy.net>
2011-01-19 00:22:55 +1100

    first commit

---
+++ lib/App/XLog.pm
@@ -0,0 +1,44 @@
+package App::XLog;
+use 5.008003;
+use Mouse;
+extends 'Cog::Plugin';
+
+our $VERSION = '0.01';
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+App::XLog - XLog Application
+
+=head1 SYNOPSIS
+
+    > cd content/dir
+    > xlog init
+    > xlog make
+    > xlog start
+
+=head1 STATUS
+
+This software is pre-alpha. Don't use it yet.
+
+=head1 DESCRIPTION
+
+XLog lets you publish your public brain in a reusable way.
+
+=head1 AUTHOR
+
+Ingy döt Net <ingy@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2011. Ingy döt Net.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=cut
---
+++ lib/App/XLog/Command.pm
@@ -0,0 +1,5 @@
+package App::Log::Command;
+use Mouse;
+extends 'Cog::Command';
+
+1;
